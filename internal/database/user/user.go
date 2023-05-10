8d41b1a45c4ca73bd7da1bf54985a9e030d32a60
Suhail Gupta <suhailgupta03@gmail.com>
2023-05-10 14:30:11 +0530

    feat: ability to notify multiple slack users

--- internal/database/user/user.go
+++ internal/database/user/user.go
@@ -20,17 +20,22 @@ type GitHubOauthModel struct {
 }
 
 type UserModel struct {
-	GitHubUsername   string           `bson:"git_hub_username" json:"git_hub_username"`
-	GitHubUserId     int64            `bson:"git_hub_user_id" json:"git_hub_user_id"`
-	Email            string           `bson:"email" json:"email"`
-	GitHubUserOauth  GitHubOauthModel `bson:"git_hub_user_oauth" json:"git_hub_user_oauth"`
-	GitHubApp        GitHubAppModel   `bson:"git_hub_app" json:"git_hub_app"`
-	SlackAccessToken *string          `json:"slack_access_token,omitempty" bson:"slack_access_token,omitempty"`
-	SlackUserId      *string          `json:"slack_user_id,omitempty" bson:"slack_user_id,omitempty"`
-	CreatedAt        int64            `bson:"created_at" json:"created_at"`
-	UpdatedAt        int64            `bson:"updated_at" json:"updated_at"`
+	GitHubUsername     string                `bson:"git_hub_username" json:"git_hub_username"`
+	GitHubUserId       int64                 `bson:"git_hub_user_id" json:"git_hub_user_id"`
+	Email              string                `bson:"email" json:"email"`
+	GitHubUserOauth    GitHubOauthModel      `bson:"git_hub_user_oauth" json:"git_hub_user_oauth"`
+	GitHubApp          GitHubAppModel        `bson:"git_hub_app" json:"git_hub_app"`
+	SlackAccessToken   *string               `json:"slack_access_token,omitempty" bson:"slack_access_token,omitempty"`
+	SlackUserId        *string               `json:"slack_user_id,omitempty" bson:"slack_user_id,omitempty"`
+	GithubSlackMapping *[]GithubSlackMapping `json:"github_slack_mapping,omitempty" bson:"github_slack_mapping,omitempty"`
+	CreatedAt          int64                 `bson:"created_at" json:"created_at"`
+	UpdatedAt          int64                 `bson:"updated_at" json:"updated_at"`
 }
 
+type GithubSlackMapping struct {
+	GitHubUsername string `bson:"git_hub_username" json:"git_hub_username"`
+	SlackUserId    string `bson:"slack_user_id" json:"slack_user_id"`
+}
 type User struct {
 	Collection *mongo.Collection
 }
@@ -78,21 +83,51 @@ func (u *User) UpdateSlackConfig(githubUserName, token, slackUserId string) erro
 	}
 
 	toUpdate := map[string]interface{}{
-		"$set": map[string]string{
+		"$set": map[string]interface{}{
 			"slack_access_token": token,
 			"slack_user_id":      slackUserId,
+			"updated_at":         time.Now().Unix(),
 		},
 	}
 	r := u.Collection.FindOneAndUpdate(ctx, where, toUpdate, nil)
 	return r.Err()
 }
 
+func (u *User) CreateNewSlackUsers(installationId int64, mapping []GithubSlackMapping) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	where := map[string]interface{}{
+		"git_hub_app.installation_id": installationId,
+	}
+
+	toUpdate := map[string]interface{}{
+		"$addToSet": map[string]interface{}{
+			"github_slack_mapping": map[string][]GithubSlackMapping{
+				"$each": mapping,
+			},
+		},
+		"$set": map[string]interface{}{
+			"updated_at": time.Now().Unix(),
+		},
+	}
+
+	r := u.Collection.FindOneAndUpdate(ctx, where, toUpdate, nil)
+	return r.Err()
+}
 func (u *User) FindUserByGitHubUsername(githubUserName string) (*UserModel, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	where := map[string]string{
-		"git_hub_username": githubUserName,
+	where := map[string]interface{}{
+		"$or": []map[string]interface{}{
+			{"git_hub_username": githubUserName},
+			{"github_slack_mapping": map[string]interface{}{
+				"$elemMatch": map[string]string{
+					"git_hub_username": githubUserName,
+				},
+			}},
+		},
 	}
 
 	r := u.Collection.FindOne(ctx, where)
@@ -101,5 +136,6 @@ func (u *User) FindUserByGitHubUsername(githubUserName string) (*UserModel, erro
 	}
 	var user UserModel
 	r.Decode(&user)
+
 	return &user, nil
 }
